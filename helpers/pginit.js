738172f933d96d411dbe5f75daec3751b5b187fe
LischukAndry <lischuk.andry@gmail.com>
2017-08-19 18:54:11 +0300

    add more inserts

--- helpers/pginit.js
+++ helpers/pginit.js
@@ -9,7 +9,7 @@ const client = new pg.Client(conString);
 client.connect();
 client.query(`CREATE TABLE IF NOT EXISTS users(
     id              serial        PRIMARY KEY,
-    fullname        varchar(64)   NOT NULL,
+    fullname        varchar(64)   UNIQUE        NOT NULL,
     userrole        varchar(20)   NOT NULL,
     passhesh        varchar(40)   NOT NULL,
     salt            varchar(16)   NOT NULL,
@@ -23,12 +23,12 @@ client.query(`CREATE TABLE IF NOT EXISTS users(
 
 client.query(`CREATE TABLE IF NOT EXISTS recipes_category(
     id            serial        PRIMARY KEY,
-    name        varchar(100)  NOT NULL,
+    name        varchar(100)    UNIQUE        NOT NULL,
     is_deleted    boolean       NOT NULL
 )`);
 client.query(`CREATE TABLE IF NOT EXISTS recipes(
     id            serial        PRIMARY KEY,
-    title         varchar(100)  NOT NULL,
+    title         varchar(100)  UNIQUE        NOT NULL,
     description   text          NOT NULL,
     is_deleted    boolean       NOT NULL,
     owner_id      integer       references users(id),
@@ -38,7 +38,7 @@ client.query(`CREATE TABLE IF NOT EXISTS recipes(
 )`);
 client.query(`CREATE TABLE IF NOT EXISTS ingradients(
     id            serial        PRIMARY KEY,
-    name          varchar(30)   NOT NULL,
+    name          varchar(30)   UNIQUE      NOT NULL,
     is_deleted    boolean       NOT NULL,
     photo         varchar(100)   
 )`);
@@ -57,7 +57,7 @@ client.query(`CREATE TABLE IF NOT EXISTS ingradient_map(
 )`);
 client.query(`CREATE TABLE IF NOT EXISTS tags(
     id           serial         PRIMARY KEY,
-    name         varchar(30)    NOT NULL
+    name         varchar(30)    UNIQUE      NOT NULL
 )`);
 client.query(`CREATE TABLE IF NOT EXISTS resipe_tag(
     id              serial      PRIMARY KEY,
@@ -76,26 +76,45 @@ client.query(`INSERT INTO users (fullname, userrole,passhesh, salt, email, is_pr
 client.query(`INSERT INTO users (fullname, userrole,passhesh, salt, email, is_premium, phone_number, is_deleted, avatar, aboutMe) VALUES
 ('GarrRoss', 'user','ba79baeb9f10896a46ae74715271b7f586e74640', 'sflprt49fhi2',  'JohnSRosa@armyspy.com', FALSE, '+1-202-555-1337', FALSE, 'none', 'Im happy!');`);
 client.query(`INSERT INTO users (fullname, userrole,passhesh, salt, email, is_premium, phone_number, is_deleted, avatar, aboutMe) VALUES
-  ('StepanGiga', 'user','ba79baeb9f10896a46ae74715271b7f586e74640', 'sflprt49fhi2',  'JohnSRosa@armyspy.com', TRUE, '+1-420-555-0101', FALSE, 'none', 'Im happy!');`);
+('StepanGiga', 'user','ba79baeb9f10896a46ae74715271b7f586e74640', 'sflprt49fhi2',  'JohnSRosa@armyspy.com', TRUE, '+1-420-555-0101', FALSE, 'none', 'Im happy!');`);
 client.query(`INSERT INTO users (fullname, userrole,passhesh, salt, email, is_premium, phone_number, is_deleted, avatar, aboutMe) VALUES
 ('MyhailoPoplavskiy', 'user','ba79baeb9f10896a46ae74715271b7f586e74640', 'sflprt49fhi2',  'JohnSRosa@armyspy.com', TRUE, '+8-800-555-35-35', FALSE, 'none', 'Im happy!');`);
 
-client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES
-('Перші страви', FALSE);`);
-client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES
-('Другі страви', FALSE);`);
-client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES
-('Десерти', FALSE);`);
-client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES
-('Напої', FALSE);`);
-client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES
-('Додатки', FALSE);`)
+client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Перші страви', FALSE);`);
+client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Другі страви', FALSE);`);
+client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Десерти', FALSE);`);
+client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Напої', FALSE);`);
+client.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Додатки', FALSE);`)
 
-client.query(`INSERT INTO resipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES
-('СИРНИКИ З МАНКОЮ', 'Сир потрібно перетерти через сито, щоб не було грудочок і тісто вийшло більш однорідним. Додаємо до сиру цукор, манку, дрібку солі й перемішуємо за допомогою вилки або блендера. У разі, якщо сир був дещо сухим, додайте в отриману масу 1 ст. ложку молока або води.Тісто вийде трохи липким, тому візьміть на руки трохи борошна, щоб легше було формувати кульки. Розігрійте пательню з невеликою кількістю рослинного масла. Обсмажуйте сирники на середньому вогні близько 7 хвилин з кожного боку, накривши кришкою. Подавайте сирники гарячими, додавши сметану або варення.','1', '3', 'https://rud.ua/uploads/under_recipe/120131111838-120530010908-p-O-sirniki-s-izjumom-bez-muki.jpg', 5,)`
-  , (err, res) => {
-    console.log(err, res);
-    client.end();
-  });
+client.query(`INSERT INTO tags (name) VALUES ('Італійська кухня');`);
+client.query(`INSERT INTO tags (name) VALUES ('Легко готувати');`);
+client.query(`INSERT INTO tags (name) VALUES ('Швидко готувати');`);
+client.query(`INSERT INTO tags (name) VALUES ('Солоденьке');`);
+client.query(`INSERT INTO tags (name) VALUES ('Гаряче');`);
+client.query(`INSERT INTO tags (name) VALUES ('Холодне');`);
+
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Цукор', 'FALSE', 'https://agropolit.com/media/news/original/00/02/2375/2216-real-693x463-3887.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Вода','FALSE', 'http://unity.lviv.ua/wp-content/uploads/2015/12/383930225.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Хліб','FALSE', 'http://senfil.net/uploads/posts/2014-10/1414486993_1547.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Майонез','FALSE', 'http://zakluchenie.com/wp-content/uploads/2016/11/maj.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Цибуля','FALSE', 'http://ukraineclub.net/sites/default/files/field/pdf/zybulya.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Морква','FALSE', 'http://likarski-roslini.net.ua/wp-content/uploads/2014/08/morkva-yak-l%D1%96karskij-zas%D1%96b.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Гриби','FALSE', 'https://adrenaline.name/uploads/posts/2016-09/thumbs/1474149239_griby-kopy.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Олія','FALSE', 'http://likarski-roslini.net.ua/wp-content/uploads/2014/11/pravilnij-vib%D1%96r-sonyashnikovoi-ol%D1%96i.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Вино','FALSE', 'https://bit.ua/wp-content/uploads/2015/03/DSC_0523.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Картопля','FALSE', 'http://infoherbs.com.ua/wp-content/uploads/2015/04/kartofel.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Вершки','FALSE', 'http://molokoferma.com.ua/73-large_default/vershki.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Мука','FALSE', 'http://cdn.bolshoyvopros.ru/files/users/images/cf/53/cf53ea864d66bce837da326792b99d35.png');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Сіль','FALSE', 'https://baker-group.net/images/v-evrope-zapretili-sol-s-iodatom-kaliya.jpg.pagespeed.ce.jKqxkj4eRB.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Перець','FALSE', 'http://okean-vkysa.com.ua/wp-content/uploads/2015/09/-----------------------6.jpg');`);
+client.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Мишʼяк','TRUE', '');`);
+
+client.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES 
+('КРЕМ-СУП У ХЛІБНОМУ ГОРЩИКУ', 'Цибулю, моркву і гриби обсмажити на олії. Влити вино і випарувати його наполовину. Потім потрібно додати воду і картоплю. Коли страва буде готова, збити блендером, посолити і поперчити. Вилити вершки і залишити ще на 10 хвилин. З хліба зрізати вершечок, вийняти мякуш і запекти в духовці до утворення скоринки. Залити крем-суп у попередньо натертий часником хліб.', FALSE, 3, 3, 'https://rud.ua/uploads/under_recipe/sup-v-hlebe.jpg', 2);`);  
+client.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES 
+('Хліб з майонезом', 'Порізати хліб, намазати на хліб майонез.', FALSE, 3, 1, 'https://i12.fotocdn.net/s9/30/public_pin_m/242/2268393757.jpg', 5);`);
+client.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES 
+('Чай', 'Насипати чай в кружку, залити гарячою водою, якщоу вас немає діабету можна додати цукор за смаком.', FALSE, 1, 5, 'http://www.fresher.ru/manager_content/images2/7-lyubopytnyx-faktov-o-granenom-stakane/big/3.jpg', 3);`);
+client.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES 
+('Вареники', 'Висипати свіжовпольовані вареники в гарячу воду, варити доки не злипнуться. bellissimo! Будь який шеф-повар буде в захваті!', FALSE, 2, 5, 'dickinchocolate.jpg', 3);`),(err, res) => {console.log(err, res);client.end();});
 }();
-

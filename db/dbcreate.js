f1544c84965711d6c798319808d065673bbb6d91
LischukAndry <lischuk.andry@gmail.com>
2017-08-24 18:00:38 +0300

    Comment tables and fielsd, delete table recipe_category

--- db/dbcreate.js
+++ db/dbcreate.js
@@ -11,31 +11,80 @@ db.query(`CREATE TABLE IF NOT EXISTS users_roles(
   else {console.log('Table users_role created!')}
 });
 
+db.query(`COMMENT ON TABLE users_roles IS 'Table with names of users roles'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users_roles.id IS 'ID number of user_role'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users_roles.user_role IS 'Name of user_role'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+
 db.query(`CREATE TABLE IF NOT EXISTS users(
     id              serial        PRIMARY KEY,
     fullname        varchar(64)   UNIQUE                        NOT NULL,
-    user_role       integer       references    users_roles(id)       NOT NULL,
+    role_id      integer       references    users_roles(id)       NOT NULL,
     passhesh        varchar(40)   NOT NULL,
     email           varchar(50)   NOT NULL,
     is_premium      boolean       NOT NULL,
     phone_number    varchar(24)   NOT NULL,
     is_deleted      boolean       NOT NULL      DEFAULT TRUE,
     gravatar        varchar(100),
-    aboutMe         text
+    about_me         text
 );`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log('Table users created!')}
-}); 
+});
 
-db.query(`CREATE TABLE IF NOT EXISTS recipes_category(
-    id            serial        PRIMARY KEY,
-    name        varchar(100)    UNIQUE        NOT NULL,
-    is_deleted    boolean       NOT NULL
-)`,
+db.query(`COMMENT ON TABLE users IS 'Table with main information about users'`,
 (err, res) => {
   if (err) {console.log(err)}
-  else {console.log('Table recipes_category created!')}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.id IS 'User ID number'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.fullname IS 'User firstname and lastname'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.role_id IS 'ID number of user role'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.passhesh IS 'Crypted  presentation of user password'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.email IS 'User email adress'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.is_premium IS 'Status of premium account'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.phone_number IS 'User contact phone number'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.is_deleted IS 'Status of user acount if value TRUE user deactiveted'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});;
+db.query(`COMMENT ON COLUMN users.gravatar IS 'Link to user avatar picture'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN users.about_me IS 'Additional information about user'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
 });
 
 db.query(`CREATE TABLE IF NOT EXISTS recipes(
@@ -44,7 +93,6 @@ db.query(`CREATE TABLE IF NOT EXISTS recipes(
     description   text          NOT NULL,
     is_deleted    boolean       NOT NULL,
     owner_id      integer       references users(id),
-    category_id   integer       references recipes_category(id),
     photo         varchar(200),
     rating        integer
 )`,
@@ -53,7 +101,36 @@ db.query(`CREATE TABLE IF NOT EXISTS recipes(
   else {console.log('Table recipes created!')}
 });
 
-db.query(`CREATE TABLE IF NOT EXISTS ingradients(
+db.query(`COMMENT ON TABLE recipes IS 'Table that consists cooking instructions and info about dishes'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN recipes.id IS 'Recipe ID number'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN recipes.title IS 'Name of dish'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN recipes.description IS 'Cookimg instruction'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN recipes.is_deleted IS 'Status of recipe if TRUE recipe dont show'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN recipes.photo IS 'Link to dish photo'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN recipes.rating IS 'Recipe quality assessment'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+
+db.query(`CREATE TABLE IF NOT EXISTS ingredients(
     id            serial        PRIMARY KEY,
     name          varchar(30)   UNIQUE      NOT NULL,
     is_deleted    boolean       NOT NULL,
@@ -61,22 +138,49 @@ db.query(`CREATE TABLE IF NOT EXISTS ingradients(
 )`,
 (err, res) => {
   if (err) {console.log(err)}
-  else {console.log('Table ingradients created!')}
+  else {console.log('Table ingredients created!')}
+});
+
+db.query(`COMMENT ON TABLE ingredients IS 'Table with list of ingredients'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN ingredients.id IS 'Ingredient ID number'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN ingredients.name IS 'Ingredient name'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN ingredients.is_deleted IS 'If TRUE dont show'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN ingredients.photo IS 'Link to ingredient photo'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
 });
 
 db.query(`CREATE TABLE IF NOT EXISTS calc_card(
     id              serial    PRIMARY KEY,
     resipe_id       serial  references recipes(id),
-    ingredient_id   serial  references ingradients(id)
+    ingredient_id   serial  references ingredients(id)
 )`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log('Table calc_card created!')}
 });
 
-db.query(`CREATE TABLE IF NOT EXISTS ingradient_map(
+db.query(`COMMENT ON TABLE calc_card IS 'Create many-to-many relation between tables resipes and ingredients'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+
+
+db.query(`CREATE TABLE IF NOT EXISTS ingredient_map(
     id              serial          PRIMARY KEY,
-    ingredient_id   integer         references ingradients(id), 
+    ingredient_id   integer         references ingredients(id), 
     is_deleted      boolean         DEFAULT     FALSE,
     lat             real,
     lon             real,
@@ -85,6 +189,23 @@ db.query(`CREATE TABLE IF NOT EXISTS ingradient_map(
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log('Table ingradient_map created!')}
+});
+
+db.query(`COMMENT ON TABLE ingredient_map IS 'Table with data about ingredients position and price'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN ingredient_map.lat IS 'Latitude'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN ingredient_map.lon IS 'Longitude'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN ingredient_map.price IS 'Actual ingredient price'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
 });
 
 db.query(`CREATE TABLE IF NOT EXISTS tags(
@@ -97,13 +218,31 @@ db.query(`CREATE TABLE IF NOT EXISTS tags(
   else {console.log('Table tags created!')}
 });
 
+db.query(`COMMENT ON TABLE tags IS 'Table with tags names and their description'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN tags.name IS 'Tag name'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+db.query(`COMMENT ON COLUMN tags.tag_description IS 'Tag description, optional field'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
+});
+
+
 db.query(`CREATE TABLE IF NOT EXISTS resipe_tag(
     id              serial      PRIMARY KEY,
-    resipe_id       integer     references ingradients(id),
+    resipe_id       integer     references ingredients(id),
     tag_id          integer     references tags(id)
 )`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log('Table recipe_tag created!')}
+});
+db.query(`COMMENT ON TABLE calc_card IS 'Create many-to-many relation between tables resipes and tags'`,(err, res) => {
+  if (err) {console.log(err)}
+  else {console.log('Table users created!')}
 });
 }();
--- db/dbseed.js
+++ db/dbseed.js
@@ -14,58 +14,33 @@ db.query(`INSERT INTO users_roles (user_role) VALUES ('admin');`,
   else {console.log(res)}
 });
 
-db.query(`INSERT INTO users (fullname, user_role, passhesh, email, is_premium, phone_number, is_deleted, gravatar, aboutMe) VALUES
+db.query(`INSERT INTO users (fullname, role_id, passhesh, email, is_premium, phone_number, is_deleted, gravatar, about_me) VALUES
 ('JohnRosauser', 1, 'ba79baeb9f10896a46ae74715271b7f586e74123', 'JohnSRosa@armyspy.com', FALSE, '+1-202-322-0101', FALSE, 'none', 'Im happy!');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
 
-db.query(`INSERT INTO users (fullname, user_role,passhesh, email, is_premium, phone_number, is_deleted, gravatar, aboutMe) VALUES
+db.query(`INSERT INTO users (fullname, role_id, passhesh, email, is_premium, phone_number, is_deleted, gravatar, about_me) VALUES
 ('GarrRoss', 1, 'ba79baeb9f10896a46ae74715271b7f586e74456', 'JohnSRosa@armyspy.com', FALSE, '+1-202-555-1337', FALSE, 'none', 'Im happy!');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
 
-db.query(`INSERT INTO users (fullname, user_role,passhesh, email, is_premium, phone_number, is_deleted, gravatar, aboutMe) VALUES
+db.query(`INSERT INTO users (fullname, role, passhesh, email, is_premium, phone_number, is_deleted, gravatar, about_me) VALUES
 ('StepanGiga', 1, 'ba79baeb9f10896a46ae74715271b7f586e74789', 'JohnSRosa@armyspy.com', TRUE, '+1-420-555-0101', FALSE, 'none', 'Im happy!');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO users (fullname, user_role,passhesh, email, is_premium, phone_number, is_deleted, gravatar, aboutMe) VALUES
+db.query(`INSERT INTO users (fullname, role_id, passhesh, email, is_premium, phone_number, is_deleted, gravatar, about_me) VALUES
 ('MyhailoPoplavskiy', 1, 'ba79baeb9f10896a46ae74715271b7f586e74999', 'JohnSRosa@armyspy.com', TRUE, '+8-800-555-35-35', FALSE, 'none', 'Im happy!');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
 
-db.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Перші страви', FALSE);`,
-(err, res) => {
-  if (err) {console.log(err)}
-  else {console.log(res)}
-});
-db.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Другі страви', FALSE);`,
-(err, res) => {
-  if (err) {console.log(err)}
-  else {console.log(res)}
-});
-db.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Десерти', FALSE);`,
-(err, res) => {
-  if (err) {console.log(err)}
-  else {console.log(res)}
-});
-db.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Напої', FALSE);`,
-(err, res) => {
-  if (err) {console.log(err)}
-  else {console.log(res)}
-});
-db.query(`INSERT INTO recipes_category (name, is_deleted) VALUES ('Додатки', FALSE);`,
-(err, res) => {
-  if (err) {console.log(err)}
-  else {console.log(res)}
-});
 
 db.query(`INSERT INTO tags (name) VALUES ('Італійська кухня');`,
 (err, res) => {
@@ -98,102 +73,102 @@ db.query(`INSERT INTO tags (name) VALUES ('Холодне');`,
   else {console.log(res)}
 });
 
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Цукор', 'FALSE', 'https://agropolit.com/media/news/original/00/02/2375/2216-real-693x463-3887.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Цукор', 'FALSE', 'https://agropolit.com/media/news/original/00/02/2375/2216-real-693x463-3887.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Вода','FALSE', 'http://unity.lviv.ua/wp-content/uploads/2015/12/383930225.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Вода','FALSE', 'http://unity.lviv.ua/wp-content/uploads/2015/12/383930225.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Хліб','FALSE', 'http://senfil.net/uploads/posts/2014-10/1414486993_1547.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Хліб','FALSE', 'http://senfil.net/uploads/posts/2014-10/1414486993_1547.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Майонез','FALSE', 'http://zakluchenie.com/wp-content/uploads/2016/11/maj.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Майонез','FALSE', 'http://zakluchenie.com/wp-content/uploads/2016/11/maj.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Цибуля','FALSE', 'http://ukraineclub.net/sites/default/files/field/pdf/zybulya.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Цибуля','FALSE', 'http://ukraineclub.net/sites/default/files/field/pdf/zybulya.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Морква','FALSE', 'http://likarski-roslini.net.ua/wp-content/uploads/2014/08/morkva-yak-l%D1%96karskij-zas%D1%96b.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Морква','FALSE', 'http://likarski-roslini.net.ua/wp-content/uploads/2014/08/morkva-yak-l%D1%96karskij-zas%D1%96b.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Гриби','FALSE', 'https://adrenaline.name/uploads/posts/2016-09/thumbs/1474149239_griby-kopy.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Гриби','FALSE', 'https://adrenaline.name/uploads/posts/2016-09/thumbs/1474149239_griby-kopy.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Олія','FALSE', 'http://likarski-roslini.net.ua/wp-content/uploads/2014/11/pravilnij-vib%D1%96r-sonyashnikovoi-ol%D1%96i.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Олія','FALSE', 'http://likarski-roslini.net.ua/wp-content/uploads/2014/11/pravilnij-vib%D1%96r-sonyashnikovoi-ol%D1%96i.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Вино','FALSE', 'https://bit.ua/wp-content/uploads/2015/03/DSC_0523.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Вино','FALSE', 'https://bit.ua/wp-content/uploads/2015/03/DSC_0523.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Картопля','FALSE', 'http://infoherbs.com.ua/wp-content/uploads/2015/04/kartofel.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Картопля','FALSE', 'http://infoherbs.com.ua/wp-content/uploads/2015/04/kartofel.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Вершки','FALSE', 'http://molokoferma.com.ua/73-large_default/vershki.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Вершки','FALSE', 'http://molokoferma.com.ua/73-large_default/vershki.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Мука','FALSE', 'http://cdn.bolshoyvopros.ru/files/users/images/cf/53/cf53ea864d66bce837da326792b99d35.png');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Мука','FALSE', 'http://cdn.bolshoyvopros.ru/files/users/images/cf/53/cf53ea864d66bce837da326792b99d35.png');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Сіль','FALSE', 'https://baker-group.net/images/v-evrope-zapretili-sol-s-iodatom-kaliya.jpg.pagespeed.ce.jKqxkj4eRB.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Сіль','FALSE', 'https://baker-group.net/images/v-evrope-zapretili-sol-s-iodatom-kaliya.jpg.pagespeed.ce.jKqxkj4eRB.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Перець','FALSE', 'http://okean-vkysa.com.ua/wp-content/uploads/2015/09/-----------------------6.jpg');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Перець','FALSE', 'http://okean-vkysa.com.ua/wp-content/uploads/2015/09/-----------------------6.jpg');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO ingradients (name, is_deleted, photo) VALUES ('Мишʼяк','TRUE', '');`,
+db.query(`INSERT INTO ingredients (name, is_deleted, photo) VALUES ('Мишʼяк','TRUE', '');`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
 
-db.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES 
-('КРЕМ-СУП У ХЛІБНОМУ ГОРЩИКУ', 'Цибулю, моркву і гриби обсмажити на олії. Влити вино і випарувати його наполовину. Потім потрібно додати воду і картоплю. Коли страва буде готова, збити блендером, посолити і поперчити. Вилити вершки і залишити ще на 10 хвилин. З хліба зрізати вершечок, вийняти мякуш і запекти в духовці до утворення скоринки. Залити крем-суп у попередньо натертий часником хліб.', FALSE, 3, 3, 'https://rud.ua/uploads/under_recipe/sup-v-hlebe.jpg', 2);`,
+db.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, photo, rating) VALUES 
+('КРЕМ-СУП У ХЛІБНОМУ ГОРЩИКУ', 'Цибулю, моркву і гриби обсмажити на олії. Влити вино і випарувати його наполовину. Потім потрібно додати воду і картоплю. Коли страва буде готова, збити блендером, посолити і поперчити. Вилити вершки і залишити ще на 10 хвилин. З хліба зрізати вершечок, вийняти мякуш і запекти в духовці до утворення скоринки. Залити крем-суп у попередньо натертий часником хліб.', FALSE, 3, 'https://rud.ua/uploads/under_recipe/sup-v-hlebe.jpg', 2);`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });  
-db.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES 
-('Хліб з майонезом', 'Порізати хліб, намазати на хліб майонез.', FALSE, 3, 1, 'https://i12.fotocdn.net/s9/30/public_pin_m/242/2268393757.jpg', 5);`,
+db.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, photo, rating) VALUES 
+('Хліб з майонезом', 'Порізати хліб, намазати на хліб майонез.', FALSE, 3, 'https://i12.fotocdn.net/s9/30/public_pin_m/242/2268393757.jpg', 5);`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES 
-('Чай', 'Насипати чай в кружку, залити гарячою водою, якщоу вас немає діабету можна додати цукор за смаком.', FALSE, 1, 5, 'http://www.fresher.ru/manager_content/images2/7-lyubopytnyx-faktov-o-granenom-stakane/big/3.jpg', 3);`,
+db.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, photo, rating) VALUES 
+('Чай', 'Насипати чай в кружку, залити гарячою водою, якщоу вас немає діабету можна додати цукор за смаком.', FALSE, 1, 'http://www.fresher.ru/manager_content/images2/7-lyubopytnyx-faktov-o-granenom-stakane/big/3.jpg', 3);`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}
 });
-db.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, category_id, photo, rating) VALUES 
-('Вареники', 'Висипати свіжовпольовані вареники в гарячу воду, варити доки не злипнуться. bellissimo! Будь який шеф-повар буде в захваті!', FALSE, 2, 5, 'dickinchocolate.jpg', 3);`,
+db.query(`INSERT INTO recipes (title, description, is_deleted, owner_id, photo, rating) VALUES 
+('Вареники', 'Висипати свіжовпольовані вареники в гарячу воду, варити доки не злипнуться. bellissimo! Будь який шеф-повар буде в захваті!', FALSE, 2, 'dickinchocolate.jpg', 3);`,
 (err, res) => {
   if (err) {console.log(err)}
   else {console.log(res)}

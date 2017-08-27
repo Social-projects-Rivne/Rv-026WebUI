eb9b0d1cfc5a305d1d844f8b71f60133f90629b3
Iurii Faevskii <urfaevskii@gmail.com>
2017-08-27 18:28:36 +0300

    added opportunity to insert recipe(all fields and path image) to db

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
 import multiparty from 'multiparty';
+import fs from 'fs';
 import uuid from 'node-uuid';
 
 import db from '../db';
@@ -22,19 +22,43 @@ recipeController.createRecipe = (req, res, next) => {
   let form = new multiparty.Form();
   
   form.parse(req, (err, fields, files) => {
-    let { path: tempPath, originalFilename } = files.photo[0];
-    let copyToPath = "../public/images/" + originalFilename;
-    let imageName = uuid.v4();
-    let fullPath = `public/images/${imageName}.jpg`;
-    fs.readFile(tempPath, (err, data) => {
-      fs.writeFile(fullPath, data, (err) => {
-        fs.unlink(tempPath, () => {
-          res.send("File uploaded");
-        });
-      }); 
-    }); 
-  });
+    if (err) {
+      return next(err); 
+    } else {
+      let { path: tempPath, originalFilename, headers} = files.photo[0];
+      var availableHeaderTypes = "image/jpeg|image/png|image/gif";
+      var fileExtension = originalFilename.split('.').pop();
+      if(availableHeaderTypes.includes(headers['content-type']))
+      {
+        let copyToPath = "../public/images/" + originalFilename;
+        let imageName = uuid.v4();
+        let fullPath = `public/images/${imageName}.${fileExtension}`;
+        fs.readFile(tempPath, (err, data) => {
+          fs.writeFile(fullPath, data, (err) => {
+            fs.unlink(tempPath, () => {
 
+              var recipeObject = new recipeModel(
+                fields.title[0], 
+                fields.description[0], 
+                false, 
+                Number(fields.owner_id[0]),
+                fullPath,
+                Number(fields.rating[0])
+              );
+
+              db.query(recipeObject.saveRecipe(recipeObject), (err, result) => {
+                if (err) {
+                  return next(err); 
+                } else {
+                  res.send({"message": "Recipe created!"});
+                }
+              });
+            });
+          }); 
+        }); 
+      } 
+    }
+  });
 };
 
 module.exports = recipeController;
--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,14 +1,21 @@
-let recipeModel = {};
+class recipeModel {
+  constructor (title, description, is_deleted, owner_id, photo, rating) {
+    this.title = title;
+    this.description = description;
+    this.is_deleted = is_deleted;
+    this.owner_id = owner_id;
+    this.photo = photo;
+    this.rating = rating;
+  };
 
-recipeModel.saveRecipe = (recipe) => {
-    var recipeValues = Object.keys(recipe).map((k) => recipe[k]);
-
-    const query = {
-      text: 'INSERT INTO recipes (title, description, is_deleted, owner_id, photo, rating) VALUES($1, $2, $3, $4, $5, $6)',
-      values: recipeValues
-    }
-    
-    return query;
-};
+  saveRecipe (recipe) {
+      var recipeValues = Object.keys(recipe).map((k) => recipe[k]);
+      const query = {
+        text: 'INSERT INTO recipes (title, description, is_deleted, owner_id, photo, rating) VALUES($1, $2, $3, $4, $5, $6)',
+        values: recipeValues
+      }
+      return query;
+  };
+}
 
 module.exports = recipeModel;

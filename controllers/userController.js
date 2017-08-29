be72e581514e0487fd4d26f695ee3b1dcd330678
Yana Shchebyvok <yshchebyvok@gmail.com>
2017-08-29 22:51:04 +0300

    db changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,16 @@
 import path from 'path';
-import pg from 'pg';
+import db from '../db';
 
-import config from '../pg_config';
 import userModel from '../models/userModel';
-import roleModel from '../models/roleModel';
-
-const conString   = config.str;
-const client      = new pg.Client(conString);
-client.connect();
 
 let userController = {};
 
 userController.getUserInfo = (req, res) => {
     //let userId = req.params.id; // need a method for the current user ID
-    let userId = 3;
-    client.query(userModel.getUserInfo(userId), (err,result) => {
+    let userId = 1;
+    db.query(userModel.getUserInfo(userId), (err,result) => {
     	if (result.rows) {
-        let queries = [];
-
-        // user role section
-
-        let roleId = result.rows[0].role_id;
-        queries.push(client.query(roleModel.getRoleName(roleId)));
-
-        Promise.all(queries).then(values => {
-          result.rows[0].user_role = values[0].rows[0].user_role; // user role name
           return res.json(result);
-        });
-
-        // end user role section
-
     	}
     	else{
     		console.log(err);
--- models/roleModel.js
+++
@@ -1,8 +0,0 @@
-
-let roleModel = {};
-
-roleModel.getRoleName = (params) => {
-      return `SELECT user_role FROM users_roles WHERE id='${params}'`;
-};
-
-module.exports=roleModel;

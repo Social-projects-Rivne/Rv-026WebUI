535f09da617ce7b5f5e1f8dc0b55304764235aa5
Iurii Faevskii <urfaevskii@gmail.com>
2017-09-10 19:02:48 +0300

    added test navbar

--- static/js/layouts/Footer.js
+++ static/js/layouts/Footer.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
+const footer = {
+    display: 'inline-block'
+}
+
+const footerWhole = {
+    padding:'10px',
+    textAlign: 'center',
+    backgroundColor: '#333',
+    color: 'white'
+}
+
+const footerLinks = {
+    color:'white'
+}
+
+
+
 class Footer extends Component {
     render(){
         return(
-            <footer>
-                <h1>Footer</h1>
+            <footer style={footerWhole}>
+                <ul style = {footer}>
+                    <li><a style={footerLinks} href="#">About us</a></li>
+                    <li><a style={footerLinks} href="#">Order now!</a></li>
+                </ul>
+                <ul style = {footer}>
+                    <li><a style={footerLinks} href="#">Recipes</a></li>
+                    <li><a style={footerLinks} href="#">How it works</a></li>
+                </ul>
+                <ul style = {footer}>
+                    <li><a style={footerLinks} href="#">Our services</a></li>
+                    <li><a style={footerLinks} href="#">Prices</a></li>
+                </ul>
+                <ul style = {footer}>
+                    <li><a style={footerLinks} href="#">Contact Us</a></li>
+                    <li><a style={footerLinks} href="#">Pricy Police</a></li>
+                </ul>
             </footer>
         );
     }
--- static/js/layouts/Header.js
+++ static/js/layouts/Header.js
@@ -1,26 +1,62 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import { browserHistory, Link } from 'react-router';
+import {
+    Button,
+    ButtonGroup,
+    ButtonToolbar,
+    Navbar,
+    NavbarBrand,
+    Collapse,
+    Nav,
+    NavItem,
+    MenuItem
+} from 'react-bootstrap';
 
 class Header extends Component {
     handleClick(e) {
         e.preventDefault();
         axios.get('/api/logout')
-        .then(res => {
-            browserHistory.push('/');
-        })
-        .catch(err => {
-            console.log(err.stack);
-            console.log('Failed to log out');
-        });
+            .then(res => {
+                browserHistory.push('/');
+            })
+            .catch(err => {
+                console.log(err.stack);
+                console.log('Failed to log out');
+            });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <header>
-                <h1>Header</h1>
-                <Link className='btn btn-default' onClick={this.handleClick}>Log out</Link>
-
+                <Navbar inverse collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Foodmake.com</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <NavItem componentClass="span">
+                                <Link className='btn btn-default' to="/recipes">Recipes</Link>
+                            </NavItem>
+                            <NavItem componentClass="span">Order now</NavItem>
+                        </Nav>
+                        <Nav pullRight>
+                            <NavItem componentClass="span">
+                                <Link className='btn btn-default' to="/signin">Login</Link>
+                            </NavItem>
+                            <NavItem componentClass="span">
+                                <Link className='btn btn-default' to="/signup">Sign up</Link>
+                            </NavItem>
+                            <NavItem href="#">Premium account</NavItem>
+                            <NavItem componentClass="span" >
+                                <Link className='btn btn-default' onClick={this.handleClick}>Log out</Link>
+                            </NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </header>
         );
     }

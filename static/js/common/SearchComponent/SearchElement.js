b3dd44ea7fed765807b4a2a603a7b2ec0d8e31c6
LischukAndry <lischuk.andry@gmail.com>
2017-09-17 19:18:21 +0300

    add SearchElementRating with stars to SearchElement Component

--- static/js/common/SearchComponent/SearchElement.js
+++ static/js/common/SearchComponent/SearchElement.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
-
+import SearchElementRating from './SearchElementRating';
 
 class SearchElement extends Component {
     constructor(props) {
@@ -13,13 +13,17 @@ class SearchElement extends Component {
 
     render() {
         const element = this.props.element;
+        
         return (
             <div key={element.id} className="search-element" onClick={() => this.onClick(element.id)}>
                 <img className="image" src={element.photo} />
-                <div className="text">
-                    <p>{element.title}</p>
-                    <p>Рейтинг:{element.rating}</p>
+                <div className="text"> 
+                    <p className="title">{element.title}</p>
+                    <p className="author">Автор: {element.fullname}</p>
+                   
                 </div>
+                
+                <SearchElementRating stars={element.rating}/>
             </div>
         );
     }
---
+++ static/js/common/SearchComponent/SearchElementRating.js
@@ -0,0 +1,34 @@
+import React, {Component} from  'react';
+
+class SearchElementRating extends Component {
+    constructor(props){
+        super(props);
+        console.log(props);
+    }
+
+    render(){
+        var stars =[];
+        var inputStars=this.props.stars
+        for (var i = 0; i < 5; i++) {
+            if(inputStars<=i){
+                stars.push(
+                    <span key={i}>{'\u2606'}</span>
+                );
+            }
+            else
+            {   stars.push(
+                <span key={i}>{'\u2605'}</span>
+                );
+            }
+            
+        }
+        return(
+            
+            <div className='element-rating'>
+                {stars.length > 0 ? <span>{stars}</span>: null}
+            </div>
+        );
+    }
+}
+
+export default SearchElementRating;

466ed6fec89971b402dcac61625d04cea9ca4f17
anton <a.pasashkov@gmail.com>
2017-09-18 15:19:49 +0300

    added main page

--- static/js/layouts/Header.js
+++ static/js/layouts/Header.js
@@ -1,25 +1,53 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import { browserHistory, Link } from 'react-router';
+import {Button, Form, FormGroup, FormControl, ButtonGroup, ButtonToolbar, Navbar, NavbarBrand, Collapse, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 class Header extends Component {
     handleClick(e) {
         e.preventDefault();
         axios.get('/api/logout')
-        .then(res => {
-            browserHistory.push('/');
-        })
-        .catch(err => {
-            console.log(err.stack);
-            console.log('Failed to log out');
-        });
+            .then(res => {
+                browserHistory.push('/');
+            })
+            .catch(err => {
+                console.log(err.stack);
+                console.log('Failed to log out');
+            });
     }
 
     render(){
         return(
             <header>
-                <h1>Header</h1>
-                <Link className='btn btn-default' onClick={this.handleClick}>Log out</Link>
+                <Navbar inverse collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Foodmake.com</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <NavItem eventKey={1} href="#">About Us</NavItem>
+                            <NavItem eventKey={2} href="#">How it works</NavItem>
+                            <NavItem eventKey={3} href="#">Recipes</NavItem>
+                            <NavItem eventKey={4} href="#">Order now</NavItem>
+                            <NavDropdown eventKey={5} title="Dropdown" id="basic-nav-dropdown">
+                                <MenuItem eventKey={5.1}>Action</MenuItem>
+                                <MenuItem eventKey={5.2}>Another action</MenuItem>
+                                <MenuItem eventKey={5.3}>Something else here</MenuItem>
+                                <MenuItem divider />
+                                <MenuItem eventKey={5.3}>Separated link</MenuItem>
+                            </NavDropdown>
+                            </Nav>
+
+                        <Nav pullRight>
+                            <NavItem eventKey={1} href="#">Login</NavItem>
+                            <NavItem eventKey={2} onClick={this.handleClick}>Log out</NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+
 
             </header>
         );
--- static/js/pages/MainPage/MainPage.js
+++ static/js/pages/MainPage/MainPage.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import TestComponentOnMainPage from './TestComponentOnMainPage';
 
-class MainPage extends Component {  
-   render() {
-      return (
-         <div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore consequatur, odio facilis? 
-                Illo similique architecto voluptatem, asperiores! 
-                Quo minus explicabo delectus modi unde, recusandae 
-                facere aperiam at quis eius. Soluta!
-            </p>
-            <TestComponentOnMainPage />
-         </div>
-      );
-   }
+const bannerBlock = {
+    fontSize:'80px',
+    color:'white',
+    marginLeft:'150px',
+    paddingRight:'20px'
+}
+
+const comment = {
+    color:'white',
+    textAlign: 'center',
+    fontSize:'30px',
+    marginTop:'-30px',
+    paddingRight:'20px'
+}
+
+const background = {
+    background: "url('public/New Project.png')",
+    height:'884px',
+    textAlign: 'center'
+}
+
+
+class MainPage extends Component {
+    render(){
+        return(
+            <div style={background}>
+                <p style={bannerBlock}>FOOD MAKE & DELIEVERY</p>
+                <p style={comment}>forget about hunger forever with FM&D!</p>
+            </div>
+
+        );
+    }
 }
 
 export default MainPage;
+
+

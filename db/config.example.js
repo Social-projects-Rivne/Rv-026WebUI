424412db3c90791b40bbd96750af9c6796eb8f7e
DmytroNovik <dimanovik19@gmail.com>
2017-10-01 00:17:47 +0300

    added config.example.js

---
+++ db/config.example.js
@@ -0,0 +1,13 @@
+const userName = 'username';
+const password = 'password';
+const dbServerLocation = 'localhost';
+const serverPort = '5432';
+const dbName = 'dbname';
+
+module.exports = {
+    userName,
+    password,
+    dbServerLocation,
+    serverPort,
+    dbName,
+};
